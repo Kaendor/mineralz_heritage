93b00d21fa06208e734fcdb81939f3270f60dd6e
Kaendor <dimteboul@gmail.com>
2026-06-15 18:19:24 +0200

    fix doc

--- src/demo/movement.rs
+++ src/demo/movement.rs
@@ -1,25 +1,6 @@
-//! Handle player input and translate it into movement through a character
-//! controller. A character controller is the collection of systems that govern
-//! the movement of characters.
-//!
-//! In our case, the character controller has the following logic:
-//! - Set [`MovementController`] intent based on directional keyboard input.
-//!   This is done in the `player` module, as it is specific to the player
-//!   character.
-//! - Apply movement based on [`MovementController`] intent and maximum speed.
-//! - Wrap the character within the window.
-//!
-//! Note that the implementation used here is limited for demonstration
-//! purposes. If you want to move the player in a smoother way,
-//! consider using a [fixed timestep](https://github.com/bevyengine/bevy/blob/main/examples/movement/physics_in_fixed_timestep.rs).
-
 use bevy::prelude::*;
 
 pub(super) fn plugin(_app: &mut App) {}
-
-/// These are the movement parameters for our character controller.
-/// For now, this is only used for a single player, but it could power NPCs or
-/// other players as well.
 
 #[derive(Component, Reflect)]
 pub struct Footprint(pub UVec2);
